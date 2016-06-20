e23aa540c723d2e67841fd3de102cb58780be517
andy fang <andy@mingtak.com.tw>
2016-06-20 12:51:51 +0100

    fixed to Compatible plone 4

--- src/mingtak/gallery/viewlet/viewlet.py
+++ src/mingtak/gallery/viewlet/viewlet.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 from plone.app.layout.viewlets import common as base
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone import api
 
 
 class Gallery(base.ViewletBase):
     """  """
+    def portal(self):
+        return api.portal.get()
+
 
 class Carousel(base.ViewletBase):
     """  """
+    def portal(self):
+        return api.portal.get()
+
